cfb81b5f6a3d36947edb933d6aadd9676aa3dec6
aloycwl <aloycwl@gmail.com>
2022-05-28 22:03:04 +0800

    4 files modified

--- js/ce.js
+++ js/ce.js
@@ -1,1 +1,17 @@
 //Casino Essential
+$(document).ready(function () {
+  setInterval(async function () {
+    if (typeof ethereum != 'undefined') {
+      d = await web3.getAccounts();
+      if (d.length > 0) {
+        $('#connect').hide();
+        $('#root').show();
+        b = (await contract2.methods.balanceOf(acct[0]).call()) / 1e18;
+        if (b != balance) {
+          balance = b;
+          refreshInfo();
+        }
+      } else $('#connect').show();
+    } else $('#connect').html('No Metamask');
+  }, 1000);
+});

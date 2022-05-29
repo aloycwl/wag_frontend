459835acc1a3b37a4f7ecae2d5eea0c8e0c7a456
aloycwl <aloycwl@gmail.com>
2022-05-29 12:32:35 +0800

    2 files modified

--- js/ce.js
+++ js/ce.js
@@ -6,69 +6,8 @@ const WB =
     'https://eth-rinkeby.alchemyapi.io/v2/xneL9EV87zUlVocEVcyDT5tqp4LZE0Fy',
   CA2 = '0xFf53E86755fddadFB671a338d4D5b3CacD9c07c1',
   CHAIN = 4;
-
-u1 = {
-  internalType: 'uint256',
-  name: '',
-  type: 'uint256',
-};
-u2 = {
-  internalType: 'uint256[]',
-  name: '',
-  type: 'uint256[]',
-};
-u3 = {
-  internalType: 'address',
-  name: '',
-  type: 'address',
-};
-u4 = {
-  internalType: 'address[]',
-  name: '',
-  type: 'address[]',
-};
 function waitTxt(a) {
   $('#load').html(a > 0 ? 'Loading...' : '');
-}
-async function LB() {
-  //Load Balance
-  return (await contract2.methods.balanceOf(acct).call()) / 1e18;
-}
-async function load(a, b) {
-  if (typeof ethereum != 'undefined') {
-    web3a = new Web3(WB);
-    web3a = web3a.eth;
-    web3 = new Web3(ethereum);
-    web3 = web3.eth;
-    acct = await ethereum.request({ method: 'eth_requestAccounts' });
-    acct = acct[0];
-    frm = { from: acct };
-    if ((await web3.net.getId()) != CHAIN) {
-      await ethereum.request({
-        method: 'wallet_switchEthereumChain',
-        params: [{ chainId: '0x' + CHAIN }],
-      });
-      location.reload();
-    }
-    contract = new web3.Contract(a, b);
-    contract = contract.methods;
-    contracta = new web3a.Contract(a, b);
-    contracta = contracta.methods;
-  }
-}
-async function load2() {
-  contract2 = new web3a.Contract(
-    [
-      {
-        inputs: [u3],
-        name: 'balanceOf',
-        outputs: [u1],
-        stateMutability: 'view',
-        type: 'function',
-      },
-    ],
-    CA2
-  );
 }
 $(document).ready(function () {
   setInterval(async function () {
